ba60ad4a340f8f2b7c53168e27777cf50f8122e2
Amir <amir_dawd@hotmail.com>
2019-05-13 07:18:39 +0200

    Working step counter.

--- app/src/main/java/com/example/sustaintrain/StepCounter.java
+++ app/src/main/java/com/example/sustaintrain/StepCounter.java
@@ -1,45 +1,57 @@
 package com.example.sustaintrain;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 
-public class StepCounter extends Activity implements SensorEventListener {
-    private int step;
-    private TextView  counter;
+public class StepCounter extends AppCompatActivity implements SensorEventListener,StepListener {
+    private int steps;
+    private TextView counter;
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private static final String TEXT_NUM_STEPS = "Steps: ";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.step_counter);
-        counter= (TextView) findViewById(R.id.stepCounterView);
+        counter = (TextView) findViewById(R.id.stepCounterView);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        simpleStepDetector = new StepDetector();
+        simpleStepDetector.registerListener(this);
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Sensor sensor = event.sensor;
-        float[] values = event.values;
-        int value = -1;
-
-        if (values.length > 0) {
-            value = (int) values[0];
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
         }
 
 
-        if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
-            step++;
-            counter.setText(step);
-        }
-        }
-
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+    public void step(long timeNs) {
+        steps++;
+        counter.setText(TEXT_NUM_STEPS + steps);
     }
+
 }
 
