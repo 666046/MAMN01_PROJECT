f6e882c3e6a9dd2265030ae8d428816caf533ca3
filkar <fkarabeleski@gmail.com>
2019-05-20 12:33:09 +0200

    Changed Button to ImageButton

--- app/src/main/java/com/example/sustaintrain/MainActivity.java
+++ app/src/main/java/com/example/sustaintrain/MainActivity.java
@@ -5,10 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
-    private Button button2;
+    private ImageButton button2;
     private Button mapButton;
 
 
@@ -16,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button= (Button) findViewById(R.id.button3);
-        button.setOnClickListener(new View.OnClickListener(){
+        /*button= (Button) findViewById(R.id.button3);
+     button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 openSaveTheWorld();
             }
-        });
-        button2 = (Button) findViewById(R.id.button4);
+        });*/
+        button2 = (ImageButton) findViewById(R.id.button4);
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
