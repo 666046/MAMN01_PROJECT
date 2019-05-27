079d1da86445685f210dab57a1aba8b7e9a1a026
filkar <fkarabeleski@gmail.com>
2019-05-27 15:59:47 +0200

    GPS implemented

--- app/src/main/java/com/example/sustaintrain/MapsActivity.java
+++ app/src/main/java/com/example/sustaintrain/MapsActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.LightingColorFilter;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -19,7 +20,10 @@ import android.widget.Toast;
 
 import com.example.sustaintrain.directionhelpers.FetchURL;
 import com.example.sustaintrain.directionhelpers.TaskLoadedCallback;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -33,8 +37,10 @@ import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private GoogleMap mMap;
     private MarkerOptions place1, place2, place3, currentPosition;
     private RadioButton route1, route2, route3;
@@ -43,17 +49,52 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Polyline currentPolyline;
     private Location currentLocation;
     private Button pickRoute;
-
+    private LatLng testLatLng;
+    private GoogleApiClient googleApiClient;
+    double lat1, lng1, lat2, lng2, lat3, lng3;
+    double totalLat, totalLng;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
-        currentPosition = new MarkerOptions().position(new LatLng(55.711102, 13.210311)).title("Current Location");
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+
+                            currentPosition = new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("Current Location");
+                        }
+                    }
+                });
+
+
         route1 = (RadioButton) findViewById(R.id.btnRoute1);
         route2 =(RadioButton) findViewById(R.id.btnRoute2);
         route3= (RadioButton) findViewById(R.id.btnRoute3);
         pickRoute= (Button) findViewById(R.id.pickRoute);
+
+
+
+
 
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.RGroup);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -62,25 +103,30 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if(checkedId == R.id.btnRoute1) {
                     new FetchURL(MapsActivity.this).execute(getUrl(currentPosition.getPosition(), place1.getPosition(), "walking"), "walking");
-
+                  //  totalLat = Math.abs(currentPosition.getPosition().latitude - lat1);
                 }
                 else if(checkedId == R.id.btnRoute2){
                     new FetchURL(MapsActivity.this).execute(getUrl(currentPosition.getPosition(), place3.getPosition(), "walking"), "walking");
+
                 }
                 else if(checkedId == R.id.btnRoute3){
                     new FetchURL(MapsActivity.this).execute(getUrl(currentPosition.getPosition(), place2.getPosition(), "walking"), "walking");
+
                 }
 
+
             }
+
 
         }); 
 
         pickRoute.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                openRoute();
+                    openRoute();
             }
         });
+
 
 
 
@@ -90,6 +136,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         place1 = new MarkerOptions().position(new LatLng(55.710982, 13.208511)).title("Studie Centrum");
         place2 = new MarkerOptions().position(new LatLng(55.711804, 13.210109)).title("John Ericssons väg");
         place3 = new MarkerOptions().position(new LatLng(55.709957, 13.208661)).title("Grönt o' Gott");
+        /*lat1 = place1.getPosition().latitude;
+        lng1 = place1.getPosition().longitude;
+        lat2 = place2.getPosition().latitude;
+        lng2 = place2.getPosition().longitude;
+        lat3 = place3.getPosition().latitude;
+        lng3 = place3.getPosition().longitude;*/
 
 
 
@@ -114,27 +166,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.addMarker(place3);
     }
 
-    private void fetchLastLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]
-                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-
-            return;
-        }
-        Task<Location> task = fusedLocationProviderClient.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if(location != null){
-                    currentLocation = location;
-                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude()
-                            +""+ currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
-                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapNearBy);
-                    supportMapFragment.getMapAsync(MapsActivity.this);
-                }
-            }
-        });
-    }
 
     private String getUrl(LatLng origin, LatLng dest, String directionMode) {
         // Origin of route
@@ -165,6 +196,55 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         startActivity(intent);
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop(){
+        if(googleApiClient.isConnected()){
+            googleApiClient.disconnect();
+        }
+        super.onStop();
+    }
 
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+
+
+        if(ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermission();
+        }
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location != null){
+
+                            testLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            currentPosition = new MarkerOptions().position(testLatLng).title("Current Location");
+
+                        }
+                    }
+                });
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(MapsActivity.this, new
+                String[]{ACCESS_FINE_LOCATION}, REQUEST_CODE);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
